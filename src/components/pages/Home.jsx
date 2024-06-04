14427d2fce2cb203222ac9d4b85bbeba60be9bd9
AbdulSamad676 <abdulsamad31650@gmail.com>
2024-06-04 12:55:35 +0500

    logout done

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,12 +1,24 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import CreatePost from '../CreatePost';
 import DisplayPosts from '../DisplayPosts';
+import { auth, signOut } from '../../../src/firebase';
 
 function Home(props) {
+  const navigate = useNavigate();
+  function handleLogout() {
+    signOut(auth)
+      .then(() => {
+        console.log('logout successfully');
+        navigate('/signup');
+      })
+      .catch((error) => {
+        // An error happened.
+      });
+  }
   return (
     <div>
-      {!props.userName && (
+      {!props.userName ? (
         <div className='authSection fixed top-3 right-2 w-full flex justify-end gap-2'>
           <Link
             to='/login'
@@ -22,6 +34,16 @@ function Home(props) {
           >
             SignUp
           </Link>
+        </div>
+      ) : (
+        <div className='fixed top-3 right-2 w-full flex justify-end gap-2'>
+          <button
+            onClick={handleLogout}
+            className='bg-blue-500   focus:bg-white
+           text-white focus:text-blue font-bold py-1 px-4 rounded'
+          >
+            Logout
+          </button>
         </div>
       )}
 
